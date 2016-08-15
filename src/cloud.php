3c4c60c42b15897ad9345c7123c3b86618232a03
Chang Liu <Chang Liu>
2016-08-15 11:23:36 +0800

    add comment after delete

--- src/cloud.php
+++ src/cloud.php
@@ -9,43 +9,10 @@ use \LeanCloud\CloudException;
  * Define cloud functions and hooks on LeanCloud
  */
 
-// /1.1/functions/sayHello
-Cloud::define("sayHello", function($params, $user) {
-    return "hello {$params['name']}";
-});
-
-// /1.1/functions/sieveOfPrimes
-Cloud::define("sieveOfPrimes", function($params, $user) {
-    $n = isset($params["n"]) ? $params["n"] : 1000;
-    error_log("Find prime numbers less than {$n}");
-    $primeMarks = array();
-    for ($i = 0; $i <= $n; $i++) {
-        $primeMarks[$i] = true;
-    }
-    $primeMarks[0] = false;
-    $primeMarks[1] = false;
-
-    $x = round(sqrt($n));
-    for ($i = 2; $i <= $x; $i++) {
-        if ($primeMarks[$i]) {
-            for ($j = $i * $i; $j <= $n;  $j = $j + $i) {
-                $primeMarks[$j] = false;
-            }
-        }
-    }
-
-    $numbers = array();
-    forEach($primeMarks as $i => $mark) {
-        if ($mark) {
-            $numbers[] = $i;
-        }
-    }
-    return $numbers;
-});
-
+/**
+ * 评论数量自动修改
+ */
 Cloud::afterSave("GymComment", function($obj, $user, $meta) {
-    // function can accepts optional 3rd argument $meta, which for example
-    // has "remoteAddress" of client.
     $gymId = $obj->get('gymId');
 
     $query = new Query("GymComment");
@@ -62,36 +29,20 @@ Cloud::afterSave("GymComment", function($obj, $user, $meta) {
     return ;
 });
 
-/*
 
-Cloud::onLogin(function($user) {
-    // reject blocker user for login
-    if ($user->get("isBlocked")) {
-        throw new FunctionError("User is blocked!", 123);
+Cloud::afterDelete("GymComment", function($obj, $user, $meta) {
+    $gymId = $obj->get('gymId');
+
+    $query = new Query("GymComment");
+    $query->equalTo('gymId', $gymId);
+    $total = $query->count();
+
+    $objSave = new Object('Gym', $gymId);
+    $objSave->set('comment', array($total));
+    try {
+        $objSave->save();
+    } catch (CloudException $ex) {
+        throw new FunctionError("计算评论数量失败" . $ex->getMessage());
     }
-});
-
-Cloud::onInsight(function($params) {
-    return;
-});
-
-Cloud::onVerified("sms", function($user){
-    return;
-});
-
-Cloud::beforeSave("TestObject", function($obj, $user) {
-    return $obj;
-});
-
-Cloud::beforeUpdate("TestObject", function($obj, $user) {
-    // $obj->updatedKeys is an array of keys that is changed in the request
-    return $obj;
-});
-
-Cloud::afterSave("TestObject", function($obj, $user, $meta) {
-    // function can accepts optional 3rd argument $meta, which for example
-    // has "remoteAddress" of client.
     return ;
 });
-
-*/
