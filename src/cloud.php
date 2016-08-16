4d36b23153b4ac6c680499761f5eb653bf6c7c6e
Chang Liu <Chang Liu>
2016-08-16 22:13:29 +0800

    add reset price status

--- src/cloud.php
+++ src/cloud.php
@@ -43,3 +43,42 @@ Cloud::afterDelete("GymComment", function($obj, $user, $meta) {
     changeComment($obj);
     return ;
 });
+
+/**
+ * 测试云函数
+ */
+Cloud::define("test", function($params, $user) {
+    error_log('添加测试日志');
+});
+
+/**
+ * 预定状态重置
+ */
+
+Cloud::define("resetStatus", function($params, $user) {
+    $query = new Query('GymSports');
+    $query->equalTo('orderType', '2');
+    $objSports = $query->find();
+    // 统计场地数量
+    $courtNumber = array();
+    foreach ($objSports as $sports) {
+        $courtNumber[$sports->getObjectId()] = $sports->get('courtNumber') ? $sports->get('courtNumber') : 1;
+    }
+
+    $query2 = new Query('Price');
+    $query2->equalTo('orderType', '2');
+    $objPrice = $query2->find();
+
+    $count = 0;
+    foreach ($objPrice as $price) {
+        $objSave = new Object("Price", $price->getObjectId());
+        $number = $courtNumber[$price->get('sportsId')] ? $courtNumber[$price->get('sportsId')] : 1;
+        $status_array = array();
+        for ($i=1; $i <= $court_number; $i++) { 
+            $status_array[] = array($i=>0);
+        }
+        $objSave->set("courtStatus", $status_array);
+        $count++;
+    }
+    error_log('共修改price记录'.$count.'条');
+});
