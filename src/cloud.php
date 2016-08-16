8312af852d42182a77d7f08050f148a13113d11a
Chang Liu <Chang Liu>
2016-08-16 19:43:31 +0800

    zip code to a function

--- src/cloud.php
+++ src/cloud.php
@@ -12,10 +12,7 @@ use \LeanCloud\CloudException;
 /**
  * 评论数量自动修改
  */
-function changeComment(){
-
-}
-Cloud::afterSave("GymComment", function($obj, $user, $meta) {
+function changeComment($obj){
     $gymId = $obj->get('gymId');
 
     $query = new Query("GymComment");
@@ -34,28 +31,15 @@ Cloud::afterSave("GymComment", function($obj, $user, $meta) {
     } catch (CloudException $ex) {
         throw new FunctionError("计算评论数量失败" . $ex->getMessage());
     }
+}
+
+
+Cloud::afterSave("GymComment", function($obj, $user, $meta) {
+    changeComment($obj);
     return ;
 });
 
-
 Cloud::afterDelete("GymComment", function($obj, $user, $meta) {
-    $gymId = $obj->get('gymId');
-
-    $query = new Query("GymComment");
-    $query->equalTo('gymId', $gymId);
-    $total = $query->count();
-    $obj = $query->find();
-    $star = 0;
-    foreach ($obj as $o) {
-        $star += $o->get('star');
-    }
-    $objSave = new Object('Gym', $gymId);
-    $objSave->set('comment', intval($total));
-    $objSave->set('score', $star/$total);
-    try {
-        $objSave->save();
-    } catch (CloudException $ex) {
-        throw new FunctionError("计算评论数量失败" . $ex->getMessage());
-    }
+    changeComment($obj);
     return ;
 });
